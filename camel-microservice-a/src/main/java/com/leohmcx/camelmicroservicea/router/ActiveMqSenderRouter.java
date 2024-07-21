fcd8119ea7bc85766f69b7ec1c862e15f04164bf
Leonardo Henrique Muniz Cornachione <leonardo.cornachione@dbccompany.com.br>
2024-07-21 20:25:45 -0300

    Using choice and simple language in Camel Routes

--- camel-microservice-a/src/main/java/com/leohmcx/camelmicroservicea/router/ActiveMqSenderRouter.java
+++ camel-microservice-a/src/main/java/com/leohmcx/camelmicroservicea/router/ActiveMqSenderRouter.java
@@ -3,7 +3,7 @@ package com.leohmcx.camelmicroservicea.router;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class ActiveMqSenderRouter extends RouteBuilder {
 
     @Override
--- camel-microservice-a/src/main/java/com/leohmcx/camelmicroservicea/router/MyFileRouter.java
+++ camel-microservice-a/src/main/java/com/leohmcx/camelmicroservicea/router/MyFileRouter.java
@@ -9,7 +9,14 @@ public class MyFileRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file:files/input")
-                .log("${body}")
+                .routeId("Files-Input-Route")
+                .transform().body(String.class)
+                .choice()
+                    .when(simple("${file:ext} == 'xml'")).log("[XML FILE]") // https://camel.apache.org/components/4.4.x/languages/file-language.html
+                    .when(simple("${body} contains 'USD'")).log("[NOT XML FILE - but contains USD]") // https://camel.apache.org/components/4.4.x/languages/simple-language.html#_operator_support
+                    .otherwise().log("[NOT XML FILE]")
+                .end()
+                .log("${messageHistory} ${headers.CamelFileAbsolute} ${file:absolute.path}") // https://camel.apache.org/components/4.4.x/languages/simple-language.html#_operator_support
                 .to("file:files/output");
     }
 }
