3c36ff7c5958bcc2a2725eb76e058d54c2335855
Leonardo Henrique Muniz Cornachione <leonardo.cornachione@dbccompany.com.br>
2024-07-21 10:49:18 -0300

    Unmarshalling Json message to java Bean in ActiveMQ

--- camel-microservice-a/src/main/java/com/leohmcx/camelmicroservicea/router/c/ActiveMqSenderRouter.java
+++ camel-microservice-a/src/main/java/com/leohmcx/camelmicroservicea/router/c/ActiveMqSenderRouter.java
@@ -8,8 +8,12 @@ public class ActiveMqSenderRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer:activemq?period=10000")
-                .transform().constant("My message for ActiveMQ")
+//        from("timer:activemq?period=10000")
+//                .transform().constant("My message for ActiveMQ")
+//                .log("${body}")
+//                .to("activemq:my-activemq-queue");
+
+        from("file:files/json")
                 .log("${body}")
                 .to("activemq:my-activemq-queue");
     }
--- camel-microservice-b/src/main/java/com/leohmcx/camelmicroserviceb/router/c/ActiveMqReceiverRouter.java
+++ camel-microservice-b/src/main/java/com/leohmcx/camelmicroserviceb/router/c/ActiveMqReceiverRouter.java
@@ -1,7 +1,13 @@
 package com.leohmcx.camelmicroserviceb.router.c;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Component
 public class ActiveMqReceiverRouter extends RouteBuilder {
@@ -9,6 +15,17 @@ public class ActiveMqReceiverRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("activemq:my-activemq-queue")
+                .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
                 .to("log:received-message-from-active-mq");
     }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class CurrencyExchange {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
 }
